49344dc72b0534a8f1be7dc0824ee2670fafe47f
Janus Sean Garzon <janus.garzon@usjr.edu.ph>
2023-07-05 12:19:24 +0800

    Create Authentication API for Google

---
+++ lib/apis/auth_api.dart
@@ -0,0 +1,26 @@
+import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:instant_gram/core/core.dart';
+
+class AuthApi implements IAuthApi {
+  final Account _account;
+
+  AuthApi(Account account) : _account = account;
+
+  @override
+  FutureEither<Session> loginWithGoogle({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final response = await _account.createEmailSession(
+        email: email,
+        password: password,
+      );
+      return right(response);
+    } on AppwriteException catch (e, stackTrace) {
+      return left(Failure(e.message ?? "Something went wrong", stackTrace));
+    }
+  }
+}
