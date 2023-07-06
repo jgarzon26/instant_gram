30cad6d493792290ba4c7fd5fd45a9e7a8326a2c
Janus Sean Garzon <janus.garzon@usjr.edu.ph>
2023-07-06 10:45:36 +0800

    Auto stash before merge of "GoogleAuth" and "main"

--- lib/apis/auth_api.dart
+++ lib/apis/auth_api.dart
@@ -1,32 +1,33 @@
 import 'package:appwrite/appwrite.dart';
-import 'package:appwrite/models.dart';
-import 'package:fpdart/fpdart.dart';
-import 'package:instant_gram/core/core.dart';
+import 'package:instant_gram/core/appwrite_providers.dart';
 import 'package:riverpod/riverpod.dart';
 
-final authApiProvider = Provider((ref) {
-  final account = ref.read(accountProvider);
-  return AuthApi(account);
-});
+final authApiProvider = Provider((ref) => _AuthApi(ref.read(accountProvider)));
 
-class AuthApi implements IAuthApi {
+class _AuthApi {
   final Account _account;
 
-  AuthApi(Account account) : _account = account;
+  _AuthApi(this._account);
 
-  @override
-  FutureEither<Session> loginWithGoogle({
-    required String email,
-    required String password,
-  }) async {
-    try {
-      final response = await _account.createEmailSession(
-        email: email,
-        password: password,
-      );
-      return right(response);
-    } on AppwriteException catch (e, stackTrace) {
-      return left(Failure(e.message ?? "Something went wrong", stackTrace));
-    }
+  Future<void> loginWithFacebook() async {
+    await _account.createOAuth2Session(
+      provider: 'facebook',
+      success: 'https://localhost:8080',
+      failure: 'https://localhost:8080',
+      scopes: ['public_profile'],
+    );
+  }
+
+  Future<void> loginWithGoogle() async {
+    await _account.createOAuth2Session(
+      provider: 'google',
+      success: 'https://localhost:8080',
+      failure: 'https://localhost:8080',
+      scopes: ['profile'],
+    );
+  }
+
+  Future<void> logout() async {
+    await _account.deleteSession(sessionId: 'current');
   }
 }
