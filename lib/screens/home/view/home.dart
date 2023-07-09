ca3d0e5bfdcfd88f64fa8538dd7fc43efe6b7625
Janus Sean Garzon <janus.garzon@usjr.edu.ph>
2023-07-09 22:33:20 +0800

    Appbar incomplete

--- lib/screens/home/view/home.dart
+++ lib/screens/home/view/home.dart
@@ -7,11 +7,25 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Instant Gram'),
+        title: const Text('Instant-gram!'),
+        actions: [
+          buildActionButton(
+            icon: const Icon(Icons.movie),
+            onPressed: () {},
+          ),
+        ],
       ),
       body: const Center(
         child: Text('Home'),
       ),
+    );
+  }
+
+  IconButton buildActionButton(
+      {required Icon icon, required VoidCallback onPressed}) {
+    return IconButton(
+      onPressed: onPressed,
+      icon: icon,
     );
   }
 }
