8b661b0cd836dd0c5fccd530854d00b505c6f7f5
Janus Sean Garzon <janus.garzon@usjr.edu.ph>
2023-07-10 13:46:39 +0800

    HomeScreen created

--- lib/screens/home/view/home.dart
+++ lib/screens/home/view/home.dart
@@ -1,22 +1,109 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with TickerProviderStateMixin {
+  final pageController = PageController();
+  late final TabController tabController;
+
+  final List<Map<String, dynamic>> actionButtons = [
+    {
+      "icon": Icons.movie_creation_outlined,
+      "onPressed": () {},
+    },
+    {
+      "icon": Icons.add_photo_alternate_outlined,
+      "onPressed": () {},
+    },
+    {
+      "icon": Icons.logout_outlined,
+      "onPressed": () {},
+    }
+  ];
+
+  final bottomIcons = [
+    Icons.person,
+    Icons.search,
+    Icons.home,
+  ];
+
+  @override
+  void initState() {
+    tabController = TabController(
+      length: bottomIcons.length,
+      vsync: this,
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Instant-gram!'),
-        actions: [
-          buildActionButton(
-            icon: const Icon(Icons.movie),
-            onPressed: () {},
+        actions: actionButtons
+            .map((action) => buildActionButton(
+                  icon: Icon(action["icon"]),
+                  onPressed: action["onPressed"],
+                ))
+            .toList(),
+        bottom: TabBar(
+          controller: tabController,
+          labelPadding: const EdgeInsets.symmetric(
+            vertical: 10,
           ),
-        ],
+          indicator: const UnderlineTabIndicator(
+            borderSide: BorderSide(
+              color: Colors.white12,
+              width: 4,
+            ),
+          ),
+          onTap: (page) {
+            setState(() {
+              pageController.animateToPage(
+                page,
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.easeIn,
+              );
+            });
+          },
+          tabs: bottomIcons
+              .map((icon) => Icon(
+                    icon,
+                  ))
+              .toList(),
+        ),
       ),
-      body: const Center(
-        child: Text('Home'),
+      body: PageView(
+        controller: pageController,
+        onPageChanged: (index) {
+          setState(() {
+            tabController.animateTo(
+              index,
+              duration: const Duration(
+                milliseconds: 100,
+              ),
+            );
+          });
+        },
+        children: const [
+          Center(
+            child: Text("No Photos or Videos. Upload some!"),
+          ),
+          Placeholder(),
+          Placeholder(),
+        ],
       ),
     );
   }
